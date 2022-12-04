d5e50ddadcc9d55a6a1144c9c8acd55b423ae9c0
Moishe Lettvin <moishel@gmail.com>
2022-12-04 07:02:49 -0700

    Part 4, tiny cleanup

--- day-4/part2.py
+++ day-4/part2.py
@@ -5,8 +5,7 @@ f = open('input.txt')
 c = 0
 for l in f.readlines():
     (range1,range2) = [[int(x) for x in x.split('-')] for x in l.strip().split(',')]
-    if overlaps(range1, range2) or overlaps(range2, range1):
-        print(range1, range2)
+    if overlaps(range1, range2):
         c += 1
 
 print(c)
