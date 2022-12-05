01a26e515bdd29ef5a6e5d692590d0e0ffdd336c
Moishe Lettvin <moishel@gmail.com>
2022-12-05 09:04:41 -0700

    Part 5

---
+++ day-5/part2.py
@@ -0,0 +1,34 @@
+import re
+from collections import defaultdict, deque
+f = open('input.txt')
+
+state = 0 # 0 is stacks; 1 is moves
+stacks = defaultdict(list)
+stack_count = None
+for l in f.readlines():
+    l = l[:-1]
+    if len(l) == 0:
+        state += 1
+        continue
+
+    if state == 0:
+        stack_count = int(len(l) / 4)
+        for i in range(int(len(l) / 4) + 1):
+            idx = i * 4 + 1
+            if str.isalpha(l[idx]):
+                stacks[i + 1].append(l[idx])
+    else:
+        m = re.match('move (\d+) from (\d+) to (\d+)', l)
+        if m:
+            (count, source, dest) = [int(x) for x in m.group(1,2,3)]
+            stacks[dest] = stacks[source][:count] + stacks[dest]
+            stacks[source] = stacks[source][count:]
+        else:
+            print("!!!", l)
+            exit(1)
+
+result = ''
+for i in range(stack_count + 1):
+    result += stacks[i + 1][0]
+
+print(result)
