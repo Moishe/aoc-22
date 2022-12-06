e54793f7b612263de9c9646b78b033dc28c57846
Moishe Lettvin <moishel@gmail.com>
2022-12-06 06:48:38 -0700

    Even shorter

--- day-6/concise.py
+++ day-6/concise.py
@@ -1,7 +1,4 @@
 unique_len = 14 # 4 for part 1
-
-f = open('input.txt')
-l = f.readline().rstrip()
-
+l = open('input.txt').readline().rstrip()
 subsets = filter(lambda x: len(x[1]) == unique_len, [(i + unique_len, set(l[i:i+unique_len])) for i in range(len(l) - unique_len)])
 print(list(subsets)[0][0])
