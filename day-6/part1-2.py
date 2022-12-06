659031e76191dc5312a31734981b85c736a925b7
Moishe Lettvin <moishel@gmail.com>
2022-12-06 06:09:15 -0700

    Part 6, minor mod

--- day-6/part1-2.py
+++ day-6/part1-2.py
@@ -1,15 +1,17 @@
 from collections import deque
 
+unique_len = 14 # 4 for part 1
+
 f = open('input.txt')
 l = f.readline().rstrip()
 
 prev = deque()
 for (i, c) in enumerate(l):
     prev.append(c)
-    if len(prev) > 14:
+    if len(prev) > unique_len:
         prev.popleft()
 
         s = set(prev)
-        if len(s) == 14:
+        if len(s) == unique_len:
             print(i + 1, s, prev)
             exit(0)
